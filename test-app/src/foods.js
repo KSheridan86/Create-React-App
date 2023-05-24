e5eb1919a2b3012bda9e71b44caf3ae8cc84c44b
Ken Sheridan <kensheridan86@gmail.com>
2023-05-24 16:39:52 +0100

    fruits import/export exercise

---
+++ test-app/src/foods.js
@@ -0,0 +1,5 @@
+const fruits = [
+        'apple', 'orange', 'grape', 'banana', 'pear', 'peach', 'mango', 'strawberry', 'blueberry', 'kiwi', 'pineapple', 'cherry'
+    ]
+
+export default fruits;
--- test-app/src/helpers.js
+++ test-app/src/helpers.js
@@ -1,14 +1,31 @@
-function helpful() {
-    console.log("I DID A HELP");
+// Lesson 1
+// function helpful() {
+//     console.log("I DID A HELP");
+// }
+
+// function useless() {
+//     console.log("I DID A BOO BOO");
+// }
+
+// function needless() {
+//     console.log("Listen to me!!!!!");
+// }
+
+// export default helpful;
+// export { useless, needless};
+
+// Lesson 2
+function choice(items) {
+    let index = Math.floor(Math.random() * items.length);
+    return items[index];
 }
 
-function useless() {
-    console.log("I DID A BOO BOO");
+function remove(items, item) {
+    for (let i = 0; i < items.length; i++) {
+        if (items[i] === item) {
+            return [...items.slice(0, i), ...items.slice(i + 1)];
+        }
+    }
 }
 
-function needless() {
-    console.log("Listen to me!!!!!");
-}
-
-export default helpful;
-export { useless, needless};
+export { choice, remove };
