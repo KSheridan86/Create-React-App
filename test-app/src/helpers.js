194927033718c4f6b924a2490e13b97c3f58d426
Ken Sheridan <kensheridan86@gmail.com>
2023-05-24 15:59:37 +0100

    import/export modules practice

---
+++ test-app/src/helpers.js
@@ -0,0 +1,14 @@
+function helpful() {
+    console.log("I DID A HELP");
+}
+
+function useless() {
+    console.log("I DID A BOO BOO");
+}
+
+function needless() {
+    console.log("Listen to me!!!!!");
+}
+
+export default helpful;
+export { useless, needless};
